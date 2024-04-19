1c3230307b14085a0daf4db8c8b9bbb7632c6148
Gautam3k09 <gautamgjhaj@gmail.com>
2024-04-20 02:29:08 +0530

    2nd push with fontawesome

---
+++ src/app/common-shop/common-shop.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-common-shop',
+  standalone: true,
+  imports: [],
+  templateUrl: './common-shop.component.html',
+  styleUrl: './common-shop.component.css'
+})
+export class CommonShopComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { CanvasAreaComponent } from '../canvas-area/canvas-area.component';
+import { HeaderPageComponent } from '../header-page/header-page.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderPageComponent,],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
