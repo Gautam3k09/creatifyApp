4e5791e7b5c7193854217137b87205e668cab2c9
Gautam3k09 <gautamgjhaj@gmail.com>
2025-02-22 03:09:57 +0530

    netlify code

--- environment.ts
+++ environment.ts
@@ -2,5 +2,6 @@
 export const environment = {
     // backend: 'http://localhost:443',
     backend: 'https://creatifyappbackend.onrender.com',
-    frontend: 'http://localhost:4200/'
+    // frontend: 'http://localhost:4200/'
+    frontend: 'https://createefi.netlify.app/'
 };
